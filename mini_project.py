82c9605d91677ec4ff821c392da33a8a8cd3fa61
Sourav Kumar <58542399+kumarsourav1499@users.noreply.github.com>
2022-07-17 20:47:38 +0530

    Add files via upload

---
+++ mini_project.py
@@ -0,0 +1,17 @@
+first = int(input("enter first number : "))
+operator = input("enter operator (+,-,*,/,%) : ")
+second = int(input("enter second number : "))
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+
+else:
+    print("Invalid Operation")
